27985d9534f32ba0482a8fea26ae47f34e6fa8e3
edolgikh <tongue_06@mail.ru>
2020-05-15 08:47:17 +0300

    delete route

--- static_src/components/MapModule.jsx
+++ static_src/components/MapModule.jsx
@@ -26,6 +26,7 @@ export default class MapModule extends React.Component{
 
     handleApiAvaliable = ymaps => {
         this.ymaps = ymaps;
+        console.log(this.ymaps);
     };
 
     addRoute = () => {
